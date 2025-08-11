46d50881960782a5c79ecabb2862e94e62ad6401
otosaku-ai <otosaku.dsp@gmail.com>
2025-08-11 11:56:45 +0300

    first commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OtosakuTTS-iOS",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "OtosakuTTS-iOS",
+            targets: ["OtosakuTTS-iOS"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "OtosakuTTS-iOS"),
+        .testTarget(
+            name: "OtosakuTTS-iOSTests",
+            dependencies: ["OtosakuTTS-iOS"]
+        ),
+    ]
+)
